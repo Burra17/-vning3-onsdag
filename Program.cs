869d53210b45d75dd7cf3b398793651ced88b4cf
André Pettersson <andre20030417@gmail.com>
2025-09-03 14:56:43 +0200

    ändrade text i program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Tjabba ändrade denna text");
         }
     }
 }
