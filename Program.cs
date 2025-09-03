65a05617b97b752f4faa8f56d7a4b0e0c8bc7cf1
André Pettersson <andre20030417@gmail.com>
2025-09-03 14:55:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace övning3_onsdag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
